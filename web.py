1ea304b183bee83d2145231fd6691b6746bd9606
tandong01 <105887276+tandong01@users.noreply.github.com>
2022-06-22 00:06:41 +0700

    Add files via upload

--- web.py
+++ web.py
@@ -34,6 +34,6 @@ if uploaded_file is not None:
     
     if Genrate_pred:
     
-        prediction = model.predict(img).argmax()
-        st.write("""Kết quả dự đoán của hình này là:{}""".format(classes [prediction])) 
-        st.write("Độ chính xác là:{} %".format(prediction))
+        prediction = model.predict(img)
+        st.write("""Kết quả dự đoán của hình này là:{}""".format(classes [np.argmax(prediction[0])])) 
+        st.write("Độ chính xác là:{} %".format(100*np.max(prediction[0],axis=0)))
